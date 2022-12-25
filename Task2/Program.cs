67ef81441ac62c5fbbfff5f50d8921de373c2913
artemtema <temaboychuk@gmail.com>
2022-12-25 12:21:57 +0300

    Создал папку с ДЗ

---
+++ Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿ int a, b;
+ 
+        Console.WriteLine("Введите два числа : ");
+ 
+        a = int.Parse(Console.ReadLine());
+ 
+        b = int.Parse(Console.ReadLine());
+ 
+        if (a > b)
+ 
+        {
+ 
+            Console.WriteLine("{0} наименьшее число", b);
+ 
+        }
+ 
+        else
+ 
+        {
+ 
+            Console.WriteLine("{0} наименьшее число ", a);
+ 
+        }
+ 
+        Console.ReadLine();
