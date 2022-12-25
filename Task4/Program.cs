885968fa4b92cd8001478496c3ebd25dbd9895b5
artemtema <temaboychuk@gmail.com>
2022-12-25 14:00:07 +0300

    Переименовал папку

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,11 +1,29 @@
-﻿Console.WriteLine("Введите число:");
-            int a = int.Parse(Console.ReadLine());
+﻿Console.WriteLine("Введите первое число: ");
+int firstNumber = int.Parse(Console.ReadLine());
 
-            if (a % 2 == 1)
-            {
-                Console.WriteLine("Число " + a + " является: НЕЧЁТНЫМ");
-            }
-            else
-            {
-                Console.WriteLine("Число " + a + "является: ЧЁТНЫМ");
-            }
+Console.WriteLine("Введите второе число: ");
+int secondNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+int thirdNumber = int.Parse(Console.ReadLine());
+
+if (firstNumber > secondNumber)
+{
+    if (firstNumber > thirdNumber)
+    {
+        Console.WriteLine("Максимальное число: " + firstNumber);
+    }
+    else
+    {
+        Console.WriteLine("Максимальное число: " + thirdNumber);
+    }
+}
+
+else if (secondNumber > thirdNumber)
+{
+    Console.WriteLine("Максимальное число: " + secondNumber);
+}
+else
+{
+    Console.WriteLine("Максимальное число: " + thirdNumber);
+}
